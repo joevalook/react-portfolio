45d3eb4a755979a9d2f17b800935e13698af0885
joevalook <joevalookaran@hotmail.com>
2023-02-27 17:29:55 -0500

    fixed the bounce in bug

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,36 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import JoeJ2 from '../../assets/images/JoeJ2.png'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['o', 'e', ' ', 'V','a','l','o','o','k','a','r','a','n']
+    const jobArray = ['F','u','l','l', ' ','S','t','a','c','k', ' ', 'W', 'e', 'b', ' ', 'D', 'e', 'v','e','l', 'o', 'p', 'e', 'r']
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi, <br /> I'm&nbsp; 
-                <img src={JoeJ2} alt="developer" />
-                oe Valookaran
+                <h1> 
+                <span className={letterClass}>H</span> 
+                <span className={`${letterClass} _12`}>i,</span> 
                 <br />
-                Full Stack Web Developer
+                <span className={`${letterClass} _13`}>I</span> 
+                <span className={`${letterClass} _14`}>'m</span> 
+                <img src={JoeJ2} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                index={15}
+                />
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                index={29}
+                />
                 </h1>
+                <h2> Frontend / Backend / React / Angular / NodeJS </h2>
                 <Link to="/contact" className='flat-button'> CONTACT ME </Link>
 
             </div>
