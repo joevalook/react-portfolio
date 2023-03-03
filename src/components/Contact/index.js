5a51dd67b58d5ee0f05c89dc87a6ef696af038de
joe valookaran <joe@DESKTOP-LDHS3F1.localdomain>
2023-03-03 13:46:09 -0500

    finished the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,31 +1,94 @@
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from '../Loader';
 import './index.scss'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
-      setTimeout(() => {
-          return setLetterClass('text-animate-hover')
-      }, 2000)
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 2000)
   }, [])
 
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm(
+      'service_lbsjphn',
+      'template_0pig7o8',
+      refForm.current,
+      'eLKKgFKvBAP9ZkUNv'
+    )
+      .then(function () {
+        alert('Message Successfully Sent');
+        window.location.reload(false)
+      }, function (error) {
+        alert('Failed to sent message, please try again');
+      });
+
+
+  }
   return (
     <>
-    <div className="container contact-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters 
-          letterClass={letterClass}
-            strArray={'Contact me'.split('')}
-            index={15}/>
-        </h1>
-      </div>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'Contact me'.split('')}
+              index={15} />
+          </h1>
+          <span className='tag-section' > &lt;article&gt;</span>
+          <p align="justify" className='contact-para'>
+            I am interested in a junior developement position at a company that provides a great learning culture. If you have any other questions, concerns or would like to get to know me better, please feel free to use the contact form below to contact me via the form below.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input type="email" name="email" placeholder="Email" required />
+                </li>
+                <li>
+                  <input type="text" name="subject" placeholder="Subject" required />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required></textarea>
+                </li>
+                <li>
+                  <input type="submit" className='flat-button' value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+          <span className='tag-section' > &lt;/article&gt;</span>
+        </div>
+        <div className="info-map">
+          Joe Valookaran,
+          <br />
+          Markham, Ontario
+          <br />
+          Canada
+           <br />
+          <span>joevalookaran@hotmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[43.89753182496072, -79.23972000636782]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[43.89753182496072, -79.23972000636782]}>
+              <Popup>I lives within a 1 kilometer radius from here, but if you live on the other side of the world I'd still love to chat! :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
-    </div>
-    <Loader />
+      </div>
+      <Loader />
     </>
   )
 }
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,17 +2,17 @@ import './index.scss'
 
 const Loader = () => {
   return (
-    <div class="loader">
-      <div class="circles">
-        <span class="one"></span>
-        <span class="two"></span>
-        <span class="three"></span>
-        <span class="four"></span>
+    <div className="loader">
+      <div className="circles">
+        <span className="one"></span>
+        <span className="two"></span>
+        <span className="three"></span>
+        <span className="four"></span>
       </div>
-      <div class="pacman">
-        <span class="top"></span>
-        <span class="bottom"></span>
-        <span class="left"></span>
+      <div className="pacman">
+        <span className="top"></span>
+        <span className="bottom"></span>
+        <span className="left"></span>
       </div>
     </div>
   )
