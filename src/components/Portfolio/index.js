e7500cf8ddfc8488b4b13210aa4a4d99ee67bedf
joe valookaran <joe@DESKTOP-LDHS3F1.localdomain>
2023-03-06 11:52:35 -0500

    finished website aside from portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,35 +3,54 @@ import './index.scss'
 import { faAngular, faCss3, faGitAlt, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import Loader from '../Loader';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
 
 const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
     return (
-            <>
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faNodeJs} color="#6CC24A" />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faDatabase} color="#FC8EAC" />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <>
+            <div className="container portfolio-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={"Portfolio".split("")}
+                            index={15}
+                        />
+                    </h1>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faNodeJs} color="#6CC24A" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faDatabase} color="#FC8EAC" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
                     </div>
                 </div>
             </div>
 
             <Loader />
-            </>
+        </>
     )
 }
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -19,11 +19,11 @@ const SideBar = () => (
       <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
         <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
       </NavLink>
-      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-      </NavLink>
       <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
         <FontAwesomeIcon icon={faSuitcase} color='#4d4d4e' />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
       </NavLink>
 
     </nav>
